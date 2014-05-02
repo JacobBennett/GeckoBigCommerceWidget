8a521b963f509e8e856d28744623afdd331087d7
Jacob Bennett <me@jakebennett.net>
2014-05-02 11:08:01 -0500

    First Commit

---
+++ rag.php
@@ -0,0 +1,66 @@
+<?php
+
+// COMPOSER AUTOLOAD
+require('vendor/autoload.php');
+
+// NAMESPACING
+use Bigcommerce\Api\Client as Bigcommerce;
+use Carbon\Carbon;
+
+// SETUP DAYS TO USE
+$now = Carbon::now();
+$yesterday = Carbon::now()->subDays(1);
+$last_week = Carbon::now()->subWeeks(1);
+$last_last_week = Carbon::now()->subWeeks(2);
+
+// LOAD IN CREDENTIALS FOR BC
+require('credentials.php');
+
+// CONFIGURE BIGCOMMERCE
+Bigcommerce::configure($credentials);
+Bigcommerce::setCipher('RC4-SHA');
+
+
+	// QUERY FOR ORDERS BETWEEN DATES
+	$orders = Bigcommerce::getOrders(['min_date_created' => $now->format('Y-m-d')]);
+
+	$orders2 = Bigcommerce::getOrders(['min_date_created' => $yesterday->format('Y-m-d'), 'max_date_created' => $now->format('Y-m-d')]);
+
+	$orders3 = Bigcommerce::getOrders(['min_date_created' => $last_week->format('Y-m-d')]);
+
+
+	// FUNCTION FOR SUMMING ORDER TOTALS
+	function sum($orders)
+	{
+		$order_totals = 0;
+
+		foreach ($orders as $order) {
+			// ADD ORDER AMOUNT WITHOUT TAX AND SHIPPING
+			$order_totals += $order->subtotal_ex_tax;
+		}
+
+		return $order_totals;
+	}
+
+
+	// RUN SUMS ON ORDERS
+	$totals = sum($orders);
+	$totals2 = sum($orders2);
+	$totals3 = sum($orders3);
+
+
+	// RETURN DATA FOR EACH WEEK
+	var_dump([
+			"Today" => [
+				"Amount" => $totals,
+				"Count" => count($orders)
+			],
+			"Yesterday" => [
+				"Amount" => $totals2,
+				"Count" => count($orders2)
+			],
+			"Last Week" => [
+				"Amount" => $totals3,
+				"Count" => count($orders3)
+			]
+		]);
